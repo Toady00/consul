6a9d5a562246be2301b3164620cd2ecfe2cda1f7
Brandon Dennis <toady00@gmail.com>
2016-12-02 15:30:00 -0500

    Fix wrong version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Consul.Mixfile do
 
   def project do
     [app: :consul,
-     version: "0.1.1",
+     version: "0.1.3",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
