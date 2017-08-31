d22f06b06fc4ba6febd799fb8d660bedae8fa974
Fernando Alonso <krakatoa1987@gmail.com>
2017-08-31 09:35:34 -0300

    Base Url configuration

--- lib/consul/http/client.ex
+++ lib/consul/http/client.ex
@@ -1,8 +1,10 @@
 defmodule Consul.HTTP.Client do
   use HTTPoison.Base
 
+  @base_url Application.get_env(:consul, :base_url, "localhost:8500")
+
   def process_url(url) do
-    "http://localhost:8500/#{url}"
+    "http://#{@base_url}/#{url}"
   end
 
   def process_response_body(body) do
