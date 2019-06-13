735b7ab77e756532fc87243502f13193ccf1cc42
Flavio <flavio@telnyx.com>
2019-06-13 13:31:51 -0300

    Add log to requests

--- lib/consul/http/client.ex
+++ lib/consul/http/client.ex
@@ -12,6 +12,7 @@ defmodule Consul.HTTP.Client do
     case Poison.decode(body) do
       {:ok, decoded_body} ->
         decode_body(decoded_body)
+
       _ ->
         body
     end
@@ -20,9 +21,10 @@ defmodule Consul.HTTP.Client do
   def process_request_body(body) when is_binary(body) do
     body
   end
+
   def process_request_body(body) do
     body
-    |> Poison.encode!
+    |> Poison.encode!()
   end
 
   def process_headers(headers) do
@@ -36,19 +38,38 @@ defmodule Consul.HTTP.Client do
   defp decode_body(items) when is_list(items) do
     Enum.map(items, &decode_body/1)
   end
+
   defp decode_body(%{"Value" => value} = item) when is_binary(value) do
     decoded_value = :base64.decode(value)
-    parsed_value = case Poison.Parser.parse(decoded_value) do
-      {:ok, parsed} -> parsed
-      {:error, {:invalid, _, _}} -> decoded_value
-    end
+
+    parsed_value =
+      case Poison.Parser.parse(decoded_value) do
+        {:ok, parsed} -> parsed
+        {:error, {:invalid, _, _}} -> decoded_value
+      end
+
     %{item | "Value" => parsed_value}
   end
+
   defp decode_body(item) do
     item
   end
 
   defp base_url do
     System.get_env("CONSUL_BASE_URL") || Application.get_env(:consul, :base_url, "localhost:8500")
+  end
+
+  def request(method, url, body \\ "", headers \\ [], opts \\ []) do
+    response = super(url, body, headers, opts)
+
+    Logger.info(fn ->
+      """
+      [CONSUL] #{inspect(method)} #{inspect(url)},
+      body: #{inspect(body)}, headers: #{inspect(headers)}, opts: #{inspect(opts)},
+      response: #{inspect(response)}
+      """
+    end)
+
+    response
   end
 end
